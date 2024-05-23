c3d02ddc72d7488e36290d21b9b3a10aeeaa9ca7
Rick Waasdorp <r.waasdorp@tudelft.nl>
2024-05-23 16:18:17 +0200

    updated L12-3v examples

--- example_L12_3v.m
+++ example_L12_3v.m
@@ -33,8 +33,8 @@ P.applyTGC = 1;
 P.half_width_aperture_nh_el = 20; % half aperture number of elements to use. Nb in paper is 2*nh+1
 
 % set min and max round trip to restrict search space and save time
-P.min_round_trip = 1.0e-6; % s
-P.max_round_trip = 3.0e-6; % s
+P.min_round_trip = 1.4e-6; % s
+P.max_round_trip = 2.6e-6; % s
 
 P.NR_to_use = 1; % reflections to use, specified as list (1, 1:2)
 P.t_rt_matching_layers = 0; % s, round trip time in matching layers
@@ -43,15 +43,15 @@ P.t_rt_matching_layers = 0; % s, round trip time in matching layers
 % P.NR_to_use = 1:2; % reflections to use, specified as list (1, 1:2)
 % P.t_rt_matching_layers = 1/P.Fc; % s, round trip time in matching layers
 
-% P.NDelay = 50; % number of values for delay to test
-% P.NThickness = 100; % number of values for thickness to test
-% P.NCLens = 100; % number of values for lens sound speed to test
+P.NDelay = 50; % number of values for delay to test
+P.NThickness = 100; % number of values for thickness to test
+P.NCLens = 100; % number of values for lens sound speed to test
 
-P.NDelay = 25; % number of values for delay to test
-P.NThickness = 50; % number of values for thickness to test
-P.NCLens = 50; % number of values for lens sound speed to test
+% P.NDelay = 25; % number of values for delay to test
+% P.NThickness = 50; % number of values for thickness to test
+% P.NCLens = 50; % number of values for lens sound speed to test
 
-P.ttp_values_test = linspace(-1 / P.Ftx, 4 / P.Ftx, P.NDelay) + P.ttp_guess; % s, TTP values to test in grid search
+P.ttp_values_test = linspace(-1 / P.Ftx, 3 / P.Ftx, P.NDelay) + P.ttp_guess; % s, TTP values to test in grid search
 P.thickness_values_test = linspace(0.4e-3, 0.9e-3, P.NThickness); % m, thickness values to test in grid search
 P.c_values_test = linspace(950, 1050, P.NCLens); % m/s, sound speed values to test in grid search
 
